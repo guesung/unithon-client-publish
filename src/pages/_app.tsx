3499d446414fa5a61553dbc8c4fd061aa1fa01f8
guesung <gueit214@naver.com>
2023-09-22 21:26:43 +0000

    feat: styled-components setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import GlobalStyle from "@/styles/GlobalStyles";
+import StyledComponentsRegistry from "@/styles/StyledComponentsRegistry";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <StyledComponentsRegistry>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </StyledComponentsRegistry>
+    </>
+  );
 }
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    
+    scrollbar-width: none;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "baseFont";
+  }
+  a {
+    -webkit-tap-highlight-color: transparent;
+    text-decoration: none;
+  }
+  button,
+  input,
+  textarea {
+    outline: none;
+    background: none;
+    border: none;
+  }
+  button {
+    cursor: pointer;
+  }
+  li {
+    list-style: none;
+  }
+  img {
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
+
+export default GlobalStyle;
