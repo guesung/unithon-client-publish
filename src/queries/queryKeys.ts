a5626e77314194c64b34620b76bbcd0e69b5ee3c
guesung <gueit214@naver.com>
2023-09-23 04:37:09 +0000

    fix: 대소문자 오타 수정

--- src/queries/queryKeys.ts
+++ src/queries/queryKeys.ts
@@ -2,12 +2,12 @@ import { Order, Position } from "@/types/profile";
 
 export enum QUERY_KEY_NAMESPACE {
   LOGIN = "login",
-  USER_List = "userList",
+  USER_LIST = "userList",
 }
 
 const QUERY_KEY = {
   login: [QUERY_KEY_NAMESPACE.LOGIN],
-  userList: (position: Position, order: Order) => [QUERY_KEY_NAMESPACE.USER_List, position, order],
+  userList: (position: Position, order: Order) => [QUERY_KEY_NAMESPACE.USER_LIST, position, order],
 };
 
 export default QUERY_KEY;
