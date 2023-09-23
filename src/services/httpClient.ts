9a65ec9b67da59ea032d905c26e1e9cab6e2a9cc
guesung <gueit214@naver.com>
2023-09-23 21:53:10 +0000

    chore: update api endpoint

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create();
-httpClient.defaults.baseURL = "https://b460-223-38-24-245.ngrok-free.app/";
+httpClient.defaults.baseURL = "https://6e8d-223-38-21-131.ngrok-free.app/";
 httpClient.defaults.headers["ngrok-skip-browser-warning"] = "69420";
 
 export default httpClient;
