214a92a24a917f89e79b37de6f96f9ac6fc20f7d
guesung <gueit214@naver.com>
2023-09-23 05:06:13 +0000

    feat: px -> rem

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -24,22 +24,22 @@ const Input = ({ size, value, onChange, ...arg }: Props) => {
 export default Input;
 
 const Wrapper = styled.div<{ size: Size }>`
-  padding: ${({ size }) => (size === "large" ? "14px 16px" : "13px 16px")};
-  border: 1px solid #d2d1d1;
-  border-radius: 4px;
+  padding: ${({ size }) => (size === "large" ? "1.4rem 1.6rem" : "1.3rem 1.6rem")};
+  border: 0.1rem solid #d2d1d1;
+  border-radius: 0.4rem;
   transition: border 0.2s;
 
   &:hover {
-    border: 1px solid #4473f5;
+    border: 0.1rem solid #4473f5;
   }
   &:focus-within {
-    border: 1px solid #4473f5;
+    border: 0.1rem solid #4473f5;
   }
 `;
 
 const BaseInput = styled.input<{ size: Size }>`
   width: 100%;
-  font-size: ${({ size }) => (size === "large" ? "16px" : "12px")};
+  font-size: ${({ size }) => (size === "large" ? "1.6rem" : "1.2rem")};
   font-weight: 400;
   color: #2d2d2d;
 `;
