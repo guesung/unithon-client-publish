b0257c9c57f0642ee3d9b5f26d081dddcb10cd57
guesung <gueit214@naver.com>
2023-09-23 11:29:03 +0000

    feat: httpClient 구현

--- src/queries/useUserListQuery.ts
+++ src/queries/useUserListQuery.ts
@@ -1,15 +1,15 @@
 import { useQuery } from "react-query";
 import querykeys from "./queryKeys";
 import { Order, Position, Profile } from "@/types/profile";
-import axios from "axios";
+import httpClient from "@/services/httClient";
 
 const fetch = async (position: Position, order: Order) => {
-  const { data } = await axios.get<Profile[]>("/users", { params: { position, orderType: order } });
+  const { data } = await httpClient.get<Profile[]>("/users", { params: { position, orderType: order } });
   return data;
 };
 
 const useUserListQuery = (position: Position, order: Order) => {
   return useQuery(querykeys.userList, () => fetch(position, order), {});
 };
- 
+
 export default useUserListQuery;
