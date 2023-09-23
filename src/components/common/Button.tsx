dac90ec05e0f3d25ded9bc611f40f249804ee52c
guesung <gueit214@naver.com>
2023-09-23 22:35:10 +0000

    feat : Button 애니메이션 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,22 +1,32 @@
 import { ButtonHTMLAttributes, HTMLAttributes } from "react";
 import { styled } from "styled-components";
+import { motion } from "framer-motion";
 
 type Size = "small" | "medium";
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface Props {
   size: Size;
   label: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 }
 
-const Button = ({ size, label, ...arg }: Props) => {
+const Button = ({ size, label, type, onClick, ...arg }: Props) => {
   return (
-    <BaseButton size={size} {...arg}>
+    <BaseButton
+      size={size}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.9 }}
+      type={type}
+      onClick={onClick}
+      {...arg}
+    >
       {label}
     </BaseButton>
   );
 };
 
-const BaseButton = styled.button<{ size: Size }>`
+const BaseButton = styled(motion.button)<{ size: Size }>`
   display: flex;
   justify-content: center;
   align-items: center;
